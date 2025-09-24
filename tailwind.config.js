d72b2d2785eb15720bd177222a73af98989ea44a
Theogene <igiranezatheogene4@gmail.com>
2025-09-24 10:47:19 +0200

    Add Theogene Portfolio on the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
       boxShadow: {
         'all': '0 0 10px 6px rgba(0, 0, 0, 0.25)', // custom all-around shadow
       },
+      keyframes: {
+        slowbounce: {
+          '0%, 100%': { transform: 'translateY(-8%)', animationTimingFunction: 'cubic-bezier(0.8,0,1,1)' },
+          '50%': { transform: 'translateY(0)', animationTimingFunction: 'cubic-bezier(0,0,0.2,1)' },
+        },
+      },
+      animation: {
+        slowbounce: 'slowbounce 4s infinite', // 3s = slower, adjust as needed
+      },
     },
   },
   plugins: [],
